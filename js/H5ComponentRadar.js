d3654e872e317ad750d2196c2b6e468ecdc13b6e
hajow <406653843@qq.com>
2016-05-18 22:05:31 +0800

    0.5

--- js/H5ComponentRadar.js
+++ js/H5ComponentRadar.js
@@ -2,13 +2,14 @@ function H5ComponentRadar(name,cfg) {
 	var radar = new H5ComponentBase(name,cfg);
 
 	var draw = document.createElement('canvas');
-	draw.height = 300;
+	draw.height = 900;
+	draw.width = 900;
 	// draw.style.background = 'red';
 	radar.append(draw);
 	var ctx = draw.getContext('2d');
 
 	var radarAnimation = function () {
-		ctx.clearRect(0,0,300,300);
+		ctx.clearRect(0,0,900,900);
 
 		// 圆心（150,150） r 100
 		// x 坐标 = a + Math.sin(rad) * r;
@@ -17,33 +18,33 @@ function H5ComponentRadar(name,cfg) {
 
 		// 绘制雷达背景
 		ctx.strokeStyle = '#ccc';
-		ctx.lineWidth = 1;
+		ctx.lineWidth = 3;
 
 		var t = cfg.date.name.length;
 		var rad = 2*Math.PI/t;
 		for (var i = 10; i >= 1; i--) {
 			ctx.beginPath();
-			ctx.moveTo(150+Math.sin(rad)*10*i,150+Math.cos(rad)*10*i);
+			ctx.moveTo(450+Math.sin(rad)*30*i,450+Math.cos(rad)*30*i);
 			for (var j = 0; j <= t - 1; j++) {
-				var x = Math.ceil(150 + Math.sin(rad*(j+1))*10*i);
-				var y = parseInt(150 + Math.cos(rad*(j+1))*10*i);
+				var x = Math.ceil(450 + Math.sin(rad*(j+1))*30*i);
+				var y = parseInt(450 + Math.cos(rad*(j+1))*30*i);
 				ctx.lineTo(x,y);
 
 				// 添加项目名称
-				if(at == 100 && i == 10){
+				if(at == 300 && i == 10){
 					var childName = $('<div class="H5ComponentRadar_name">' + cfg.date.name[j] + '</div>');
 					cfg.date.color[j] && childName.css('color',cfg.date.color[j]);
-					if (x==150) {
-						childName.css('left',(x-25) +'px');
-					} else if(x < 150){
-						childName.css('right',(300-x) +'px');
-					}else if(x > 150){
-						childName.css('left',x+'px');
+					if (x/3==150) {
+						childName.css('left',(x/3-25) +'px');
+					} else if(x/3 < 150){
+						childName.css('right',(300-x/3) +'px');
+					}else if(x/3 > 150){
+						childName.css('left',x/3+'px');
 					}
-					if (y<150) {
-						childName.css('bottom',(300-y) +'px');
+					if (y/3<150) {
+						childName.css('bottom',(300-y/3) +'px');
 					} else{
-						childName.css('top',y+'px');
+						childName.css('top',y/3+'px');
 					}
 					setTimeout(function (childName) {
 						return function () {
@@ -64,26 +65,29 @@ function H5ComponentRadar(name,cfg) {
 		// 绘制折线	
 		ctx.strokeStyle = '#ff7676';
 		ctx.fillStyle = '#ff7676';
-		ctx.lineWidth = 1;
+		ctx.lineWidth = 3;
 
 		var per = cfg.date.percent;
-		for (var i = 0; i <= t-1; i++) {
-			var x = 150 + Math.sin(rad*(i+1))*at*per[i];
-			var y = 150 + Math.cos(rad*(i+1))*at*per[i];
-			var nextX = 150 + Math.sin(rad*(i+2))*at*per[i+1];
-			var nextY = 150 + Math.cos(rad*(i+2))*at*per[i+1];
+		for (var i = 0; i < t; i++) {
+			var x = 450 + Math.sin(rad*(i+1))*at*per[i];
+			var y = 450 + Math.cos(rad*(i+1))*at*per[i];
+			var nextX = 450 + Math.sin(rad*(i+2))*at*per[i+1];
+			var nextY = 450 + Math.cos(rad*(i+2))*at*per[i+1];
 			ctx.beginPath();
 		    ctx.moveTo(x,y);
-			ctx.arc(x,y,2,0,2*Math.PI);
+			ctx.arc(x,y,6,0,2*Math.PI);
 			ctx.fill();
-			ctx.lineTo(nextX,nextY)
+			
 			if(i == (t-1)){
-				ctx.lineTo(150 + Math.sin(rad*(1))*at*per[0],150 + Math.cos(rad*(1))*at*per[0])
+				nextX = 450 + Math.sin(rad)*at*per[0];
+				nextY = 450 + Math.cos(rad)*at*per[0];
 			}
+			ctx.lineTo(nextX,nextY);
+			
 			ctx.stroke();
 		}
-		at++;
-		if (at <= 100) {
+		at += 5;
+		if (at <= 300) {
 			setTimeout(radarAnimation,10);
 		}
 	}
@@ -91,7 +95,7 @@ function H5ComponentRadar(name,cfg) {
 	
 	radar.on('onLoad',function () {
 		radar.show();
-		at = 1;
+		at = 0;
 		radarAnimation();
 	});
 	radar.on('onLeave',function () {
